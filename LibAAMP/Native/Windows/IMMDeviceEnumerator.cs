4f581d3ec3c2c32860cd3beabed02e8dcdc91c65
BenApplegate <applegateben03@gmail.com>
2024-09-08 13:55:32 -0600

    Added LibAAMP project

---
+++ LibAAMP/Native/Windows/IMMDeviceEnumerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LibAAMP.Native.Windows;
+
+[ComImport]
+[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6")]
+[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+public interface IMMDeviceEnumerator
+{
+    
+}
